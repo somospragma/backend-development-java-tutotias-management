a462c8d64b2d82dff41349e35cf6d3e539d14599
Anderson Castaño <andercc2880@gmail.com>
2025-09-16 13:27:54 -0500

    feat(user): add filtering functionality for users by estado, rol, and chapterId

--- src/main/java/com/pragma/usuarios/domain/port/output/UserRepository.java
+++ src/main/java/com/pragma/usuarios/domain/port/output/UserRepository.java
@@ -1,6 +1,8 @@
 package com.pragma.usuarios.domain.port.output;
 
 import com.pragma.usuarios.domain.model.User;
+import com.pragma.usuarios.domain.model.enums.RolUsuario;
+
 import java.util.Optional;
 import java.util.List;
 
@@ -10,4 +12,5 @@ public interface UserRepository {
     Optional<User> findByEmail(String email);
     Optional<User> findByGoogleUserId(String googleUserId);
     List<User> findAll();
+    List<User> findByFilters(String estado, RolUsuario rol, String chapterId);
 }
