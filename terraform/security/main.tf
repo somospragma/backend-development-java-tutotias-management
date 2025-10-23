3abdde976e73a490b16d065848eeff4a52293fc6
Felipe Arcienagas <felipe.arcienagas@FELIPE-ARCIENAGAS.local>
2025-10-23 15:10:15 -0500

    Update ALB Rules Front - Backend

--- terraform/security/main.tf
+++ terraform/security/main.tf
@@ -99,172 +99,152 @@ module "sm" {
       force_overwrite_replica_secret = true
       replica                        = []
       create_secret_version          = false
+    }  
     }
-  }
   depends_on = [module.kms]
 }
 
 ##############################################################
 # Modulo SG
 ##############################################################
-module "sg" {
-  source = "git::https://github.com/somospragma/cloudops-ref-repo-aws-sg-terraform.git?ref=feature/sg-module-mapobject"
-  providers = {
-    aws.project = aws.principal
-  }
-
-  client      = var.client
-  project     = var.project
-  environment = var.environment
-
-  sg_config = {
-    "elb" = {
-      service     = "alb"
-      application = "tutorias"
-      description = "Security group for alb tutorias"
-      vpc_id      = data.aws_vpc.vpc.id
-      additional_tags = {
-        application-tier = "backend"
-      }
-
-      ingress = [
-        {
-          from_port       = 80
-          to_port         = 80
-          protocol        = "tcp"
-          cidr_blocks     = ["181.32.109.172/32"]
-          security_groups = []
-          prefix_list_ids = []
-          self            = false
-          description     = "Allow HTTP inbound Felipe"
-        },
-        {
-          from_port       = 80
-          to_port         = 80
-          protocol        = "tcp"
-          cidr_blocks     = ["191.110.124.113/32"]
-          security_groups = []
-          prefix_list_ids = []
-          self            = false
-          description     = "Allow HTTP inbound Jammer"
-        },        
-        {
-          from_port       = 443
-          to_port         = 443
-          protocol        = "tcp"
-          cidr_blocks     = ["181.32.109.172/32"]
-          security_groups = []
-          prefix_list_ids = []
-          self            = false
-          description     = "Allow HTTP inbound Felipe"
-        },
-        {
-          from_port       = 443
-          to_port         = 443
-          protocol        = "tcp"
-          cidr_blocks     = ["191.110.124.113/32"]
-          security_groups = []
-          prefix_list_ids = []
-          self            = false
-          description     = "Allow HTTP inbound Jammer"
-        },
-      ]
-
-      egress = [
-        {
-          from_port       = 0
-          to_port         = 0
-          protocol        = "-1"
-          cidr_blocks     = ["0.0.0.0/0"]
-          prefix_list_ids = []
-          security_groups = []
-          self            = false
-          description     = "Allow all outbound traffic"
-        }
-      ]
-    },
-    "ecs" = {
-      service     = "ecs"
-      application = "tutorias"
-      description = "Security group for ecs tutorias"
-      vpc_id      = data.aws_vpc.vpc.id
-      additional_tags = {
-        application-tier = "tutorias"
-      }
-
-      ingress = [
-        {
-          from_port       = 8080
-          to_port         = 8080
-          protocol        = "tcp"
-          cidr_blocks     = []
-          security_groups = ["elb"]
-          prefix_list_ids = []
-          self            = false
-          description     = "Allow traffic on port 7008 from ALB delivery"
-        },
-        {
-          from_port       = 8080
-          to_port         = 8080
-          protocol        = "tcp"
-          cidr_blocks     = []
-          security_groups = []
-          prefix_list_ids = []
-          self            = true
-          description     = "Allow traffic on port 7008 from the same security group delivery"
-        }
-      ]
-
-      egress = [
-        {
-          from_port       = 0
-          to_port         = 0
-          protocol        = "-1"
-          cidr_blocks     = ["0.0.0.0/0"]
-          prefix_list_ids = []
-          security_groups = []
-          self            = false
-          description     = "Allow all outbound traffic"
-        }
-      ]
+  module "sg" {
+    source = "git::https://github.com/somospragma/cloudops-ref-repo-aws-sg-terraform.git?ref=feature/sg-module-mapobject"
+    providers = {
+      aws.project = aws.principal
     }
-    "rds" = {
-      service     = "rds"
-      application = "mysql"
-      description = "Security group for Aurora Mysql"
-      vpc_id      = data.aws_vpc.vpc.id
-      additional_tags = {
-        application-tier = "database"
+
+    client      = var.client
+    project     = var.project
+    environment = var.environment
+
+    sg_config = {
+      "elb" = {
+        service     = "alb"
+        application = "tutorias"
+        description = "Security group for alb tutorias"
+        vpc_id      = data.aws_vpc.vpc.id
+        additional_tags = {
+          application-tier = "backend"
+        }
+
+        ingress = [
+          {
+            from_port       = 443
+            to_port         = 443
+            protocol        = "tcp"
+            cidr_blocks     = ["0.0.0.0/0"]
+            security_groups = []
+            prefix_list_ids = []
+            self            = false
+            description     = "Allow HTTP inbound"
+          },
+          {
+            from_port       = 80
+            to_port         = 80
+            protocol        = "tcp"
+            cidr_blocks     = ["0.0.0.0/0"]
+            security_groups = []
+            prefix_list_ids = []
+            self            = false
+            description     = "Allow HTTP inbound"
+          }
+        ]
+
+        egress = [
+          {
+            from_port       = 0
+            to_port         = 0
+            protocol        = "-1"
+            cidr_blocks     = ["0.0.0.0/0"]
+            prefix_list_ids = []
+            security_groups = []
+            self            = false
+            description     = "Allow all outbound traffic"
+          }
+        ]
+      },
+      "ecs" = {
+        service     = "ecs"
+        application = "tutorias"
+        description = "Security group for ecs tutorias"
+        vpc_id      = data.aws_vpc.vpc.id
+        additional_tags = {
+          application-tier = "tutorias"
+        }
+
+        ingress = [
+          {
+            from_port       = 8080
+            to_port         = 8080
+            protocol        = "tcp"
+            cidr_blocks     = []
+            security_groups = ["elb"]
+            prefix_list_ids = []
+            self            = false
+            description     = "Allow traffic on port 7008 from ALB delivery"
+          },
+          {
+            from_port       = 8080
+            to_port         = 8080
+            protocol        = "tcp"
+            cidr_blocks     = []
+            security_groups = []
+            prefix_list_ids = []
+            self            = true
+            description     = "Allow traffic on port 7008 from the same security group delivery"
+          }
+        ]
+
+        egress = [
+          {
+            from_port       = 0
+            to_port         = 0
+            protocol        = "-1"
+            cidr_blocks     = ["0.0.0.0/0"]
+            prefix_list_ids = []
+            security_groups = []
+            self            = false
+            description     = "Allow all outbound traffic"
+          }
+        ]
       }
-
-      ingress = [
-        {
-          from_port       = 3306
-          to_port         = 3306
-          protocol        = "tcp"
-          cidr_blocks     = []
-          security_groups = ["ecs"]
-          prefix_list_ids = []
-          self            = false
-          description     = "Allow Mysql traffic from ECS security group"
+      "rds" = {
+        service     = "rds"
+        application = "mysql"
+        description = "Security group for Aurora Mysql"
+        vpc_id      = data.aws_vpc.vpc.id
+        additional_tags = {
+          application-tier = "database"
         }
-      ]
 
-      egress = [
-        {
-          from_port       = 0
-          to_port         = 0
-          protocol        = "-1"
-          cidr_blocks     = ["0.0.0.0/0"]
-          prefix_list_ids = []
-          security_groups = []
-          self            = false
-          description     = "Allow all outbound traffic"
-        }
-      ]
+        ingress = [
+          {
+            from_port       = 3306
+            to_port         = 3306
+            protocol        = "tcp"
+            cidr_blocks     = []
+            security_groups = ["ecs"]
+            prefix_list_ids = []
+            self            = false
+            description     = "Allow Mysql traffic from ECS security group"
+          }
+        ]
+
+        egress = [
+          {
+            from_port       = 0
+            to_port         = 0
+            protocol        = "-1"
+            cidr_blocks     = ["0.0.0.0/0"]
+            prefix_list_ids = []
+            security_groups = []
+            self            = false
+            description     = "Allow all outbound traffic"
+          }
+        ]
+      }
     }
   }
-}
 
 ##############################################################
 # Modulo IAM
